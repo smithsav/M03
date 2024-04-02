06dde5a2da090048d26e6d2b97ba2d673fdf504e
Savannah Smith <ssmith1695@ivytech.edu>
2024-04-02 18:36:20 -0500

    Updated

--- M03ProgramAssignListsFunctionsClasses.py
+++ M03ProgramAssignListsFunctionsClasses.py
@@ -5,7 +5,7 @@ class Vehicle:
 
 class Automobile(Vehicle):
     def __init__(self, vehicle_type, year, make, model, doors, roof):
-        super().__init__(vehicle_type)  # Initialize vehicle_type using superclass constructor
+        super().__init__(vehicle_type) 
         self.year = year
         self.make = make
         self.model = model
